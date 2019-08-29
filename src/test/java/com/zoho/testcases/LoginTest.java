f5e99c27ac4f55de84464299662763831895c7b1
Dhananjay Deshpande <ddeshpande@nexant.corp>
2019-08-30 00:35:38 +0530

    secon commit

--- src/test/java/com/zoho/testcases/LoginTest.java
+++ src/test/java/com/zoho/testcases/LoginTest.java
@@ -2,6 +2,7 @@ package com.zoho.testcases;
 
 import java.util.Hashtable;
 
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import com.zoho.base.TestBase;
@@ -15,7 +16,10 @@ public class LoginTest extends TestBase  {
 	@Test(priority =1,dataProviderClass = Utilities.class,dataProvider = "dp")
 	public void loginTest(Hashtable<String, String>data)
 	{
-		
+		if(!data.get("RNMODE").equals("Y"))
+		{
+			throw new SkipException("Test case RunMode is"+"LoginTest"+"is set to No ");
+		}
         HomoPage home = new HomoPage();
 		LoginPage lp = home.doLogin();
 		ZohoApp zp = new ZohoApp();
